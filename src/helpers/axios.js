c9a49e1ab6dc604f9ea67c2227576447fce68a99
Pranav <pranav.d@eksaq.in>
2025-07-30 23:36:07 +0530

    changes

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: "https://finhub-backend.onrender.com", // full URL of your backend
-    // withCredentials: true, // important for cookies / auth
+    withCredentials: true, // important for cookies / auth
 });
 
 export default axiosInstance;
