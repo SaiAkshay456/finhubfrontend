9ce413ad3a03ecc026b4896d5b91a97e3b4c3e11
Kartikeya-H-Mulay <kartikeya.mulay@khmengineers.in>
2025-07-31 00:17:20 +0530

    Changed localhost

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://finhub-backend.onrender.com", // full URL of your backend
+    // baseURL: "https://finhub-backend.onrender.com", // full URL of your backend
+    baseURL: "http://localhost:3000", // full URL of your backend
     withCredentials: true, // important for cookies / auth
 });
 
