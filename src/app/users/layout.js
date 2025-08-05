d945dff72c79927ce312184dedc34fad42b29533
SaiAkshay456 <saiakshayorg@gmail.com>
2025-08-05 17:02:50 +0530

    deploy

--- src/app/users/layout.js
+++ src/app/users/layout.js
@@ -11,17 +11,20 @@ export const metadata = {
 export default async function Layout({ children }) {
     const cookieStore = await cookies();
     const token = cookieStore.get('token')?.value;
+    console.log(token, "14 line")
 
     const currentPath = '/users'; // or get from route segment
-
+    console.log(token, "17 line")
     // Find label based on current path
     const matched = sidebarItems.find(item => item.path === currentPath);
     const label = matched?.label;
+    console.log(token, "2 line")
 
     if (!label) {
         redirect('/'); // or 404
     }
     try {
+        console.log(token, "27 line")
         const { data } = await axiosInstance.post('/v1/permission-route/check-access', { path: label }, {
             headers: {
                 'Content-Type': 'application/json',
@@ -29,12 +32,15 @@ export default async function Layout({ children }) {
             },
         });
         console.log(data)
+        console.log("35 line")
         if (!data.success) {
             redirect('/unauthorized');
         }
     } catch (err) {
         if (err.response?.status === 401) {
+            console.log("41 line")
             redirect('/unauthorized');
+
         } else {
             console.error('Access check failed:', err);
         }
