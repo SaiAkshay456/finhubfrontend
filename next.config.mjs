4d70c5611fc79efccf294adc3991e03aa72d1538
SaiAkshay456 <saiakshayorg@gmail.com>
2025-08-06 03:56:22 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,27 @@ const nextConfig = {
     // eslint: {
     //     ignoreDuringBuilds: true,
     // },
+    async headers() {
+        return [
+            {
+                source: "/(.*)", // apply to all routes
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Cross-Origin-Embedder-Policy",
+                        value: "require-corp",
+                    },
+                ],
+            },
+        ];
+    },
 
 };
+
+// next.config.js
 
 export default nextConfig;
 
