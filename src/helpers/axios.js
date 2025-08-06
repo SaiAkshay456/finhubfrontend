73fdd54989804abe0b9339d614c7fab2cc09a64c
SaiAkshay456 <saiakshayorg@gmail.com>
2025-08-06 13:11:47 +0530

    deploy

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -3,6 +3,9 @@ import axios from "axios";
 const axiosInstance = axios.create({
     baseURL: "https://finhub-backend.onrender.com",
     withCredentials: true,
+    headers: {
+        'Content-Type': 'application/json',
+    }
 });
 axios.defaults.withCredentials = true;
 
