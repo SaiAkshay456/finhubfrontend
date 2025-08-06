0e690a3a45f8ffc643d2dc12d426eb06326ce6ab
SaiAkshay456 <saiakshayorg@gmail.com>
2025-08-06 11:29:23 +0530

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,6 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // ✅ Required for Netlify static export
-    reactStrictMode: true,
-
-    images: {
-        unoptimized: true, // ✅ Required if you're using <Image /> without image optimization
-    },
-
     async headers() {
         return [
             {
